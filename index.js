ba3a11d4985568e69889c34b3dd0e5c2436d42b1
Onur Dogan <iamonurdogan@gmail.com>
2022-10-17 11:05:14 +0300

    add item

---
+++ index.js
@@ -0,0 +1,40 @@
+// TODO Eleman ekleme
+
+// Eleman Seçimi
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnAddNewTask = document.querySelector("#btnAddNewTask");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+
+eventListeners();
+
+function eventListeners() {
+  form.addEventListener("submit", addNewItem);
+}
+
+function addNewItem(e) {
+  if (input.value === "") {
+    alert("Please add new item");
+    //console.log('submit');
+  }
+
+  // input alanına girilen elemanı enter veya artı butonuna basıldığında listeye eklemek için li oluşturma!
+  const li = document.createElement("li");
+  li.className = "list-group-item list-group-item-secondary";
+  li.appendChild(document.createTextNode(input.value));
+
+  // a elementi oluşturmak!
+  const a = document.createElement("a");
+  a.classList = "delete-item position-absolute end-0 me-3";
+  a.setAttribute;
+  "href", "#";
+  a.innerHTML = '<i class="fas fa-times"></i>';
+
+  li.appendChild(a);
+  taskList.appendChild(li);
+
+  input.value = "";
+
+  e.preventDefault();
+}
